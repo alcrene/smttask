46f23e2e015e7ab773cb277b4b6d18e58dcc87fc
Alexandre René <alexandre.rene@freeneurons.ca>
2020-06-23 10:47:41 +0200

    Add `init` CLI command.
    Provides a wizard, smttask-compatible defaults, and some extra sanity 
    checks (e.g. that datastores are writeable).
    Still pretty basic; not all Sumatra configurations are supported yet.

--- smttask/ui.py
+++ smttask/ui.py
@@ -1,10 +1,88 @@
 import click
 import logging
 from .base import Task, config
+import os
+from pathlib import Path
+import sumatra.commands
 
 @click.group()
 def cli():
     pass
+
+@cli.command()
+def init():
+    """Run an initialization wizard for SumatraTask.
+
+    If you want to include this in a script, the underlying `smt init`
+    command provided by Sumatra may be more appropriate.
+    """
+    print(
+        "In order to ensure task results are never overwritten, while still "
+        "providing consistent file locations inputs, SumatraTask requires "
+        "separate paths for output and input (files in the 'input' directory "
+        "are symlinked to their most recent equivalent in the 'output' "
+        "directory). By default these are the subfolders './data' and "
+        "'./data/run_dump'."
+        )
+
+    cwd = Path(os.getcwd())
+    path_repo = cwd
+
+    print("\nSet the project directory. This is the git repository containing "
+          "the code and settings used for the project. "
+          "Note: providing a different value will clone the repository at "
+          "that location into the current directory."
+    r = input(f"Project directory (default {path_repo}): ")
+    if r != "":
+        path_repo = Path(r).expanduser()
+        print(f"{path_repo} will be cloned to {cwd}.")
+
+    project_name = cwd.stem
+    r = input(f"Project name (default {project_name}): ")
+    if r != "":
+        project_name = r
+
+    path_inputs = cwd/"data"
+    path_outputs = cwd/"data/run_dump"
+    r = input(f"\nInput datastore (default: {path_inputs}): ")
+    if r != "":
+        path_inputs = Path(r).expanduser()
+    r = input(f"\nOutput datastore (default: {path_outputs}): ")
+    if r != "":
+        path_outputs = Path(r).expanduser()
+
+    fail = False
+    if path_inputs.exists() and not path_inputs.is_dir():
+        print("Input datastore is not a directory.")
+        fail = True
+    if path_outputs.exists() and not path_outputs.is_dir():
+        print("Output datastore is not a directory.")
+        fail = True
+    if path_inputs == path_outputs:
+        print("Input and output datastore must be different.")
+        fail = True
+    # Check that input/output dirs are read-writable.
+    # Dirs may not be created yet, so we go up the path until we find the
+    # first already existing directory
+    for path in (path_inputs, path_outputs):
+        latest_parent = path
+        while not latest_parent.exists():
+            latest_parent = latest_parent.parent
+        if not os.access(latest_parent, os.W_OK | os.R_OK):
+            print(f"{latest_parent} is not read-writable")
+            fail = True
+    if fail:
+        return
+
+    # Construct the argument list as it would be passed on the CLI, and
+    # call Sumatra's init
+    argv_str = f"{project_name} --datapath {path_outputs} --input {path_inputs} " \
+               f"--repository {path_repo}"
+    print("This will initialize a Sumatra project with the following command:")
+    print("smt init " + argv_str)
+    r = input("Continue ? [Y/n] ")
+    if r.lower() != "n":
+        sumatra.commands.init(argv_str.split())
 
 @cli.command()
 @click.argument('taskdesc', type=click.File('r'))
