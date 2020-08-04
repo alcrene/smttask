1b9168513c8d0183b218f108ffed4b1a3fd2d250
Alexandre René <alexandre.rene@freeneurons.ca>
2020-08-04 10:29:18 +0200

    Fix Sumatra version from 0.8 to 0.8dev0

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     packages=['smttask'],
 
     install_requires=['attrdict',
-                      'sumatra[git]>=0.8',
+                      'sumatra[git]>=0.8dev0',
                       #'psycopg2',
                       'mackelab-toolbox',
                       'parameters',
