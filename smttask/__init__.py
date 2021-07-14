d1956d9ec4626679e1168f80bd6a2f38e6fa751f
Alexandre René <alexandre.rene@freeneurons.ca>
2021-07-14 15:03:35 +0200

    Workaround for conflicting Django versions
    Sumatra and Holoviews (through bokeh) require different versions of 
    Django. We use the version required by Sumatra (< 2), which works as 
    long as Holoviews is imported before Django.
    This workaround ensures that Holoviews is imported before anything else 
    in `smttask`, which should prevent most users from running into this 
    issue.

--- smttask/__init__.py
+++ smttask/__init__.py
@@ -1,3 +1,23 @@
+# Workaround for Django import conflict:
+# Sumatra requires us to use an old version of Django (< 2), which technically
+# conflicts with Bokeh (and therefore Holoviews).
+# Things seem to work, as long as Holoviews is imported before django
+# We can at least save users from remembering this import order in many cases 
+# by forcing a Holoviews import here. We don't actually want it in the namespace
+# though, so we delete it immediately (it will continue to live in sys.modules)
+from warnings import warn
+try:
+    import holoviews
+except Exception:
+    # In addition to holoviews not being installed, we may already be too late
+    # (django may already have been loaded), in which case there's nothing we
+    # can do but hope that holoviews won't be needed.
+    warn("A test import of `holoviews` failed. If you want to use `holoviews` "
+         "for plotting, remember to import it before `smttask`.")
+else:
+    del holoviews
+# End workaround    
+
 from .base import *
 from .decorators import *
 from .task_generators import *
