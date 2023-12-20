c614d1aa13bc43b2b8092d61b340e6c24d44c708
Alexandre René <alex.c.rene@gmail.com>
2023-12-19 20:41:40 -0500

    git subrepo pull vendor/sumatra
    subrepo:
      subdir:   "vendor/sumatra"
      merged:   "51b2e24"
    upstream:
      origin:   "https://github.com/alcrene/sumatra"
      branch:   "master"
      commit:   "51b2e24"
    git-subrepo:
      version:  "0.4.6"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "110b9eb"

--- vendor/sumatra/src/sumatra/recordstore/shelve_store.py
+++ vendor/sumatra/src/sumatra/recordstore/shelve_store.py
@@ -32,14 +32,18 @@ class ShelveRecordStore(RecordStore):
         # Some shelve backends add an extension to the filename, and more than one
         # file may be created. So that the file(s) can be deleted, we need to try
         # to discover the full filename(s).
-        dir = os.path.dirname(os.path.abspath(shelf_name))
-        initial_dir_contents = set(os.listdir(dir))
-        self.shelf = shelve.open(shelf_name)
-        self._shelf_files = set(os.listdir(dir)).difference(initial_dir_contents)
+        self.shelf.close()  # Open and close shelf to ensure it exists
+        shelf_dir = os.path.dirname(os.path.abspath(shelf_name))
+        initial_dir_contents = set(os.listdir(shelf_dir))
+        self._shelf_files = set(os.listdir(shelf_dir)).difference(initial_dir_contents)
 
-    def __del__(self):
-        if hasattr(self, "shelf"):
-            self.shelf.close()
+    @property
+    def shelf(self):
+        return shelve.open(self._shelf_name)
+
+    # def __del__(self):
+    #     if hasattr(self, "shelf"):
+    #         self.shelf.close()
 
     def __str__(self):
         return "Record store using the shelve package (database file=%s)" % self._shelf_name
@@ -51,68 +55,77 @@ class ShelveRecordStore(RecordStore):
         self.__init__(**state)
 
     def list_projects(self):
-        return [str(key) for key in self.shelf.keys()]
+        with self.shelf as shelf:
+            return [str(key) for key in shelf.keys()]
 
     def has_project(self, project_name):
-        return project_name in self.shelf
+        with self.shelf as shelf:
+            return project_name in shelf
 
     def save(self, project_name, record):
-        if project_name in self.shelf:
-            records = self.shelf[project_name]
-        else:
-            records = {}
-        records[record.label] = record
-        self.shelf[project_name] = records
-        self.shelf.sync()  # At least some backends don't immediately write to disk, even when writeback=False
+        with self.shelf as shelf:
+            if project_name in shelf:
+                records = shelf[project_name]
+            else:
+                records = {}
+            records[record.label] = record
+            shelf[project_name] = records
+            shelf.sync()  # At least some backends don't immediately write to disk, even when writeback=False
 
     def get(self, project_name, label):
-        return self.shelf[project_name][label]
+        with self.shelf as shelf:
+            return shelf[project_name][label]
 
     def list(self, project_name, tags=None):
-        if project_name in self.shelf:
-            if tags:
-                if not isinstance(tags, list):
-                    tags = [tags]
-                records = [record for record in self.shelf[project_name].values()
-                           if any([tag in record.tags for tag in tags])]
+        with self.shelf as shelf:
+            if project_name in shelf:
+                if tags:
+                    if not isinstance(tags, list):
+                        tags = [tags]
+                    records = [record for record in shelf[project_name].values()
+                               if any([tag in record.tags for tag in tags])]
+                else:
+                    records = list(shelf[project_name].values())
             else:
-                records = list(self.shelf[project_name].values())
-        else:
-            records = []
-        return records
+                records = []
+            return records
 
     def labels(self, project_name, tags=None):
-        if project_name in self.shelf:
-            if tags:
-                if not isinstance(tags, list):
-                    tags = [tags]
-                lbls = [label for label, record in self.shelf[project_name].items()
-                        if any([tag in record.tags for tag in tags])]
+        with self.shelf as shelf:
+            if project_name in shelf:
+                if tags:
+                    if not isinstance(tags, list):
+                        tags = [tags]
+                    lbls = [label for label, record in shelf[project_name].items()
+                            if any([tag in record.tags for tag in tags])]
+                else:
+                    lbls = list(shelf[project_name].keys())
             else:
-                lbls = list(self.shelf[project_name].keys())
-        else:
-            lbls = []
-        return lbls
+                lbls = []
+            return lbls
 
     def delete(self, project_name, label):
-        records = self.shelf[project_name]
-        records.pop(label)
-        self.shelf[project_name] = records
+        with self.shelf as shelf:
+            records = shelf[project_name]
+            records.pop(label)
+            shelf[project_name] = records
 
     def delete_by_tag(self, project_name, tag):
-        for_deletion = [record for record in self.shelf[project_name].values() if tag in record.tags]
-        for record in for_deletion:
-            self.delete(project_name, record.label)
-        return len(for_deletion)
+        with self.shelf as shelf:
+            for_deletion = [record for record in shelf[project_name].values() if tag in record.tags]
+            for record in for_deletion:
+                self.delete(project_name, record.label)
+            return len(for_deletion)
 
     def most_recent(self, project_name):
-        most_recent = None
-        most_recent_timestamp = datetime.min
-        for record in self.shelf[project_name].values():
-            if record.timestamp > most_recent_timestamp:
-                most_recent_timestamp = record.timestamp
-                most_recent = record.label
-        return most_recent
+        with self.shelf as shelf:
+            most_recent = None
+            most_recent_timestamp = datetime.min
+            for record in shelf[project_name].values():
+                if record.timestamp > most_recent_timestamp:
+                    most_recent_timestamp = record.timestamp
+                    most_recent = record.label
+            return most_recent
 
     def clear(self):
         for path in self._shelf_files:
